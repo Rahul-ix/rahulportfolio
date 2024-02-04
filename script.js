e29a3ae5bdc40435a90a4e211f2d6566274d1c41
rahul <rahulkalal676@gmail.com>
2024-02-04 10:19:31 +0530

    initial commitss

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+    // window.onload = function() {
+    //     var numberOfPages = 3;
+    //     var num = Math.round(Math.random() * numberOfPages);
+    //     window.location.href = "/" + num + ".html";
+    // };    
